cdca2c430204e79876dfd7c032d390a098025f0e
puja lakhe <lakhepuza924@gmail.com>
2025-01-13 17:28:27 +0545

    feat:register service to register new users

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User, UserResponse } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,17 +9,12 @@ import { Injectable } from '@angular/core';
 export class RegisterService {
   private apiUrl = 'http://localhost:8000/signup/'; // Your Django API endpoint
   constructor(private httpClient: HttpClient) {}
-  signUp(userData: any) {
+  signUp(userData: User): Observable<UserResponse> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
     });
-    this.httpClient.post(this.apiUrl, userData, { headers }).subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
+    return this.httpClient.post<UserResponse>(this.apiUrl, userData, {
+      headers,
+    });
   }
 }
