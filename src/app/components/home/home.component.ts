9beba9ce85dd10f196bab7b68d9041c7101060be
puja lakhe <lakhepuza924@gmail.com>
2025-01-17 22:27:43 +0545

    feat:landing page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CourseCategoryService } from '../../services/course-category.service';
+import { CommonModule } from '@angular/common';
+import { CourseCategory } from '../../models/course-category';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  courseCategories: CourseCategory[] = [];
+
+  courseCategoryService = inject(CourseCategoryService);
+  ngOnInit() {
+    this.courseCategories = this.courseCategoryService.getAll();
+  }
+  getBackgroundColor(index: number): string {
+    // Change color based on the index or any other logic
+    return index % 2 === 0 ? '#f5e9d7' : '#e1b16a';
+  }
+}
