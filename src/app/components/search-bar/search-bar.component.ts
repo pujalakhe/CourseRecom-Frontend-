d2a80f9444a44e4affba24a521e8b3b3614ee273
puja lakhe <lakhepuza924@gmail.com>
2025-01-26 14:49:47 +0545

    feat:searchbar component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Form,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HttpErrorService } from '../../services/http-error.service';
+import { SearchPageComponent } from '../search-page/search-page.component';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  searchQuery: string = '';
+  constructor(private router: Router) {}
 
+  onSearch() {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], {
+        queryParams: { search: this.searchQuery },
+      });
+      console.log(this.searchQuery);
+    }
+  }
 }
