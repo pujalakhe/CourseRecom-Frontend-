e83be016c435cc901fc73fa80991ab31f5d8396c
puja lakhe <lakhepuza924@gmail.com>
2025-02-01 18:07:49 +0545

    feat:course component

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [NavbarComponent, SearchBarComponent],
+  imports: [
+    NavbarComponent,
+    SearchBarComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss',
 })
