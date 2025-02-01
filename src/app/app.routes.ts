ab43bb3fef7abcd0305f8d31b1c8bb5b5014b073
puja lakhe <lakhepuza924@gmail.com>
2025-02-01 15:54:40 +0545

    add more components and fix the routes spellings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,13 +8,17 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { CourseComponent } from './components/course/course.component';
 import { authGuard } from './auth.guard';
 import { SearchPageComponent } from './components/search-page/search-page.component';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'course', component: CourseComponent, canActivate: [authGuard] },
+  { path: 'about', component: AboutComponent },
+  { path: 'courses', component: CourseComponent },
+  { path: 'course-details', component: CourseDetailComponent },
   { path: 'search', component: SearchPageComponent },
   { path: 'contact', component: ContactComponent, canActivate: [authGuard] },
   { path: '**', component: PageNotFoundComponent }, //wildcard
